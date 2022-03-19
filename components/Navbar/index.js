3158121512bc43a8949bc61a1e907d9a805d744d
SURESH NALOOR KOOTEEL UNNIKRISHNAN <IN1003199@JAL.LOCAL>
2022-03-19 18:50:24 +0300

    navbar index refactored'

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -10,40 +10,41 @@ const Navbar = () => {
   // When mounted on client, now we can show the UI
   useEffect(() => setMounted(true), []);
 
-  if (!mounted) return null;
-
-  if (session) {
+  if (!mounted) return null;  
     return (
         <div className="flex justify-between p-3 bg-light-secondary dark:bg-dark-secondary text-dark-primary dark:text-light-primary">
         <div className="font-bold my-auto text-lg">navigation bar</div>
 
-        <div className="flex">
+        <div >
           <button onClick={() => setTheme(theme == "dark" ? "light" : "dark")}>
             switch to {theme === "dark" ? "light" : "dark"}
           </button>
         </div>
-      <div className="flex">
-        <button onClick={() => signOut()}>sign Out {session.user.email}</button>
+      <div >
+        {session?.user ? (
+          <button onClick={() => signOut()}>sign Out {session.user.email}</button>)
+        :( <button onClick={() => signIn()}>sign in</button>)}
+        
       </div>
         </div>
     );
   }
 
-    return (
-      <div className="flex justify-between p-3 bg-light-secondary dark:bg-dark-secondary text-dark-primary dark:text-light-primary">
-        <div className="font-bold my-auto text-lg">navigation bar</div>
+//     return (
+//       <div className="flex justify-between p-3 bg-light-secondary dark:bg-dark-secondary text-dark-primary dark:text-light-primary">
+//         <div className="font-bold my-auto text-lg">navigation bar</div>
 
-        <div className="flex">
-          <button onClick={() => setTheme(theme == "dark" ? "light" : "dark")}>
-            switch to {theme === "dark" ? "light" : "dark"}
-          </button>
-        </div>
+//         <div className="flex">
+//           <button onClick={() => setTheme(theme == "dark" ? "light" : "dark")}>
+//             switch to {theme === "dark" ? "light" : "dark"}
+//           </button>
+//         </div>
 
-        <div className="flex">
-          <button onClick={() => signIn()}>sign in</button>
-        </div>
-      </div>
-    );
-}
+//         <div className="flex">
+//           <button onClick={() => signIn()}>sign in</button>
+//         </div>
+//       </div>
+//     );
+// }
 
 export default Navbar
