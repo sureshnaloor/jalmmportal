b854ca4b841eb3d9fcc9aedefd83400051af4a40
SURESH NALOOR KOOTEEL UNNIKRISHNAN <IN1003199@JAL.LOCAL>
2023-07-10 13:53:54 +0300

    year2018 deleted since started from 2019

--- components/Vendordetailed/vendorevaluationpastyears.js
+++ components/Vendordetailed/vendorevaluationpastyears.js
@@ -9,7 +9,7 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 function vendorevaluationpastyears({ vendornumber }) {
-  const [score2018, setScore2018] = useState(0);
+  const [score2022, setScore2022] = useState(0);
   const [score2019, setScore2019] = useState(0);
   const [score2020, setScore2020] = useState(0);
   const [score2021, setScore2021] = useState(0);
@@ -41,25 +41,25 @@ function vendorevaluationpastyears({ vendornumber }) {
           <div className="col-span-1">
             {" "}
             <h3 className="bg-amber-200 text-green-900 pr-9 mr-9">
-              Year 2018 Score
-            </h3>
-          </div>
-          <div className="col-span-1">
-            {" "}
-            <h3 className="bg-sky-200 text-green-900 pr-9 mr-9">
               Year 2019 Score
             </h3>
           </div>
           <div className="col-span-1">
             {" "}
+            <h3 className="bg-sky-200 text-green-900 pr-9 mr-9">
+              Year 2020 Score
+            </h3>
+          </div>
+          <div className="col-span-1">
+            {" "}
             <h3 className="bg-emerald-200 text-green-900 pr-9 mr-9">
-              Year 2020 Score{" "}
+              Year 2021 Score{" "}
             </h3>
           </div>
           <div className="col-span-1">
             {" "}
             <h3 className="bg-teal-200 text-green-900 pr-9 mr-9">
-              Year 2021 Score
+              Year 2022 Score
             </h3>
           </div>
 
@@ -70,56 +70,6 @@ function vendorevaluationpastyears({ vendornumber }) {
               {" "}
               Already updated the score:{" "}
               {vendorpastscores?.["past"]?.[0].pastyearscore}
-            </p>
-          ) : (
-            <div className="mt-3 grid grid-cols-2">
-              <input
-                type="text"
-                id="year2018"
-                onChange={(e) => setScore2018(e.target.value)}
-                className="h-[24px] col-span-1 w-1/2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50  focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              />
-
-              <button
-                onClick={async () => {
-                  // console.log("I am clicked", score2018)
-                  await fetch(
-                    `/api/vendors/vendorevalpastyears/${vendornumber}`,
-                    {
-                      method: "PUT",
-                      body: JSON.stringify({
-                        pastyear: "2018",
-                        score: score2018,
-                        createdBy: session?.user?.name,
-                        createdAt: new Date(),
-                      }),
-                      headers: new Headers({
-                        "Content-Type": "application/json",
-                        Accept: "application/json",
-                      }),
-                    }
-                  );
-
-                  toast.success(
-                    `The PAST EVALUATION SCORE FOR YEAR 2018 for the vendor ${vendornumber} is updated, thanks!`,
-                    {
-                      position: toast.POSITION.TOP_RIGHT,
-                    }
-                  );
-                  router.reload();
-                }}
-                className="h-[24px] w-1/2 col-span-1 mr-20 font-bold text-[12px] bg-blue-500 hover:bg-blue-700 text-white  rounded-full"
-              >
-                Update
-              </button>
-            </div>
-          )}
-
-          {vendorpastscores?.["past"]?.[1] ? (
-            <p>
-              {" "}
-              Already updated for 2019:
-              {vendorpastscores?.["past"]?.[1].pastyearscore}{" "}
             </p>
           ) : (
             <div className="mt-3 grid grid-cols-2">
@@ -149,19 +99,27 @@ function vendorevaluationpastyears({ vendornumber }) {
                       }),
                     }
                   );
+
+                  toast.success(
+                    `The PAST EVALUATION SCORE FOR YEAR 2019 for the vendor ${vendornumber} is updated, thanks!`,
+                    {
+                      position: toast.POSITION.TOP_RIGHT,
+                    }
+                  );
+                  router.reload();
                 }}
-                className="h-[24px] w-1/2 col-span-1 mr-20 font-bold text-[12px] bg-blue-500  hover:bg-blue-500 text-white  rounded-full"
+                className="h-[24px] w-1/2 col-span-1 mr-20 font-bold text-[12px] bg-blue-500 hover:bg-blue-700 text-white  rounded-full"
               >
                 Update
               </button>
             </div>
           )}
 
-          {vendorpastscores?.["past"]?.[2] ? (
+          {vendorpastscores?.["past"]?.[1] ? (
             <p>
               {" "}
-              The score is already updated:{" "}
-              {vendorpastscores?.["past"]?.[2].pastyearscore}
+              Already updated for 2019:
+              {vendorpastscores?.["past"]?.[1].pastyearscore}{" "}
             </p>
           ) : (
             <div className="mt-3 grid grid-cols-2">
@@ -192,18 +150,18 @@ function vendorevaluationpastyears({ vendornumber }) {
                     }
                   );
                 }}
-                className="h-[24px] w-1/2 col-span-1 mr-20 font-bold text-[12px] bg-blue-500 hover:bg-blue-700 text-white  rounded-full"
+                className="h-[24px] w-1/2 col-span-1 mr-20 font-bold text-[12px] bg-blue-500  hover:bg-blue-500 text-white  rounded-full"
               >
                 Update
               </button>
             </div>
           )}
 
-          {vendorpastscores?.["past"]?.[3] ? (
+          {vendorpastscores?.["past"]?.[2] ? (
             <p>
               {" "}
               The score is already updated:{" "}
-              {vendorpastscores?.["past"]?.[3].pastyearscore}
+              {vendorpastscores?.["past"]?.[2].pastyearscore}
             </p>
           ) : (
             <div className="mt-3 grid grid-cols-2">
@@ -224,6 +182,48 @@ function vendorevaluationpastyears({ vendornumber }) {
                       body: JSON.stringify({
                         pastyear: "2021",
                         score: score2021,
+                        createdBy: session?.user?.name,
+                        createdAt: new Date(),
+                      }),
+                      headers: new Headers({
+                        "Content-Type": "application/json",
+                        Accept: "application/json",
+                      }),
+                    }
+                  );
+                }}
+                className="h-[24px] w-1/2 col-span-1 mr-20 font-bold text-[12px] bg-blue-500 hover:bg-blue-700 text-white  rounded-full"
+              >
+                Update
+              </button>
+            </div>
+          )}
+
+          {vendorpastscores?.["past"]?.[3] ? (
+            <p>
+              {" "}
+              The score is already updated:{" "}
+              {vendorpastscores?.["past"]?.[3].pastyearscore}
+            </p>
+          ) : (
+            <div className="mt-3 grid grid-cols-2">
+              <input
+                type="text"
+                id="year2022"
+                onChange={(e) => setScore2022(e.target.value)}
+                className="h-[24px] col-span-1 w-1/2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50  focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              />
+
+              <button
+                onClick={async () => {
+                  // console.log("I am clicked", score2018)
+                  await fetch(
+                    `/api/vendors/vendorevalpastyears/${vendornumber}`,
+                    {
+                      method: "PUT",
+                      body: JSON.stringify({
+                        pastyear: "2022",
+                        score: score2022,
                         createdBy: session?.user?.name,
                         createdAt: new Date(),
                       }),
