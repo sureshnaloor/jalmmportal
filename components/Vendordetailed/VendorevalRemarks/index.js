d6355a09b3728b602493ec8ee5983b5d6a638f9a
SURESH NALOOR KOOTEEL UNNIKRISHNAN <IN1003199@JAL.LOCAL>
2023-07-24 08:18:11 +0300

    tinyMCE added in vendor evaluation remarks- working now

--- components/Vendordetailed/VendorevalRemarks/index.js
+++ components/Vendordetailed/VendorevalRemarks/index.js
@@ -1,19 +1,46 @@
-import React, {useRef} from 'react'
+import React, {useRef, useState} from 'react'
 import { Editor } from '@tinymce/tinymce-react';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function VendorevalRemarks({vendornumber}) {
     const editorRef = useRef(null);
 
-    // const handleEditorChange = (content, editor) => {
-    //   setFormData({ ...formData, description: content });
-    // }
+    const [formData, setFormData] = useState("")
+
+    const handleEditorChange = (content, editor) => {
+      setFormData({ ...formData, description: content });
+    }
   return (
     <div className="bg-zinc-100 mx-auto w-5/6 drop-shadow rounded-md mb-9">
-    <h4 className="mb-3 py-3 shadow-lg shadow-slate-200 mx-auto my-auto bg-teal-600 text-white font-bold font-italic tracking-widest">
+    <h4 className="mb-3 py-3 shadow-lg shadow-slate-200 mx-auto my-auto bg-emerald-400 text-white font-bold font-italic tracking-widest">
       {" "}
       The Evaluation REMARKS FOR THE vendor code: {vendornumber}
     </h4>
+    <form onSubmit={async (e) => {
+        e.preventDefault()
+        console.log(formData)
+        await fetch(
+            `/api/vendors/vendorevalremarks/${vendornumber}`,
+            {
+              method: "PUT",
+              body: JSON.stringify(formData),
+              headers: new Headers({
+                "Content-Type": "application/json",
+                Accept: "application/json",
+              }),
+            }
+          );
 
+          toast.success(
+            `The vendor evaluation remarks for the vendor ${vendornumber} is updated, thanks!`,
+            {
+              position: toast.POSITION.TOP_RIGHT,
+            }
+          );
+    
+    }
+}>
     <Editor
         onInit={(evt, editor) => editorRef.current = editor}
         // there's a known issue with how tinymce works where the intialValue and value
@@ -35,10 +62,13 @@ function VendorevalRemarks({vendornumber}) {
           'removeformat | help',
           content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
         }}
-        // value={formData.description}
-        // onEditorChange={handleEditorChange}
+
+        
+        value={formData.description}
+        onEditorChange={handleEditorChange}
       />
-      {/* <button onClick={log}>Log editor content</button> */}
+      <button className='bg-emerald-400 text-white px-2 rounded-lg shadow-md shadow-slate-500 align-middle justify-end'> Save</button>
+      </form>
     </div>
   )
 }
