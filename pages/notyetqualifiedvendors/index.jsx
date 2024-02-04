585fd7b4e14b92b0fc2c14940b011d5841aa0539
SURESH NALOOR KOOTEEL UNNIKRISHNAN <IN1003199@JAL.LOCAL>
2024-02-04 17:02:30 +0300

    mobile format removed in nonsap vendor registration

--- pages/notyetqualifiedvendors/index.jsx
+++ pages/notyetqualifiedvendors/index.jsx
@@ -550,7 +550,7 @@ function index() {
                     id="salesmobile"
                     className="block py-2.5 px-0 w-1/2 text-sm text-teal-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                     placeholder=" "
-                    pattern="053-123-4567"
+                    
                     {...register("salesmobile", {
                       required: "Salesperson's Mobile is required",
                     })}
