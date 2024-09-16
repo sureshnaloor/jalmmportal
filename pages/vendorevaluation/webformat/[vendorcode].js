8ca51829e027e393df498a2a734c48fcfaab6574
sureshnaloor <suresh.naloor@gmail.com>
2024-09-16 16:26:52 +0300

    vendoreval past scores included

--- pages/vendorevaluation/webformat/[vendorcode].js
+++ pages/vendorevaluation/webformat/[vendorcode].js
@@ -17,9 +17,16 @@ function vendorevaluation() {
     }, []);
   
     console.log(evalmarks.filter(evalmark => evalmark.vendorcode === vendorcode))
+    const vendoreval = evalmarks.filter(evalmark => evalmark.vendorcode === vendorcode)
+    console.log(vendoreval)
   return (
     <>
     <div> Evaluation of vendor {vendorcode} </div>
+    <h1> Past year scores </h1>
+    <p> Year {vendoreval.past[0].pastyear} <span> {vendoreval.past[0].pastyearscore} </span></p>
+    <p> Year {vendoreval.past[1].pastyear} <span> {vendoreval.past[1].pastyearscore} </span></p>
+    <p> Year {vendoreval.past[2].pastyear} <span> {vendoreval.past[2].pastyearscore} </span></p>
+    <p> Year {vendoreval.past[3].pastyear} <span> {vendoreval.past[3].pastyearscore} </span></p>
     </>
   )
 }
